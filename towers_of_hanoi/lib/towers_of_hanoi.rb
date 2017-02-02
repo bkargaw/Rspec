6d23bb8e0b7bbdaf0e787bf690501113f09b5809
App Academy Student <student@appacademy.io>
2017-02-01 16:37:49 -0800

    Finish Towers of Hanoi specs

--- towers_of_hanoi/lib/towers_of_hanoi.rb
+++ towers_of_hanoi/lib/towers_of_hanoi.rb
@@ -16,7 +16,7 @@ class TowersOfHanoi
       raise ArgumentError.new("Invalid move") if towers[start].last > towers[finish].last
     end
 
-    towers[finish] << towers[start].pop
+    @towers[finish] << @towers[start].pop
   end
 
   def won?
@@ -24,4 +24,33 @@ class TowersOfHanoi
     false
   end
 
+  def run
+    until won?
+      run_turn
+    end
+    puts "Congratulations!  You won!"
+  end
+
+  def run_turn
+    start, finish = get_move
+    move(start, finish)
+  rescue
+    puts "Invalid input"
+    retry
+  end
+
+  def get_move
+    display
+    puts "Where do you want to move the disk to: start, end"
+    gets.chomp.split(",").map { |el| Integer(el.strip) }
+  end
+
+  def display
+    puts towers.inspect
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = TowersOfHanoi.new
+  game.run
 end
